bcf54076dbc906e8ef324d0d1b1c3a36666ad472
Unknown <unknown@example.com>
2020-01-07 02:00:25 +0530

    Add Protobuf study

---
+++ src/include/libprotobuf.hpp
@@ -0,0 +1,6 @@
+#include <fstream>
+#include <google/protobuf/util/time_util.h>
+#include <iostream>
+#include <string>
+
+#include "src/lib/addressbook.pb.h"
---
+++ test/test_protobuf.cpp
@@ -0,0 +1,37 @@
+#include <libprotobuf.hpp>
+
+using namespace std;
+
+using google::protobuf::util::TimeUtil;
+void ListPeople(const tutorial::AddressBook& address_book);
+
+// Main function:  Reads the entire address book from a file and prints all
+//   the information inside.
+int main(int argc, char* argv[]) {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  if (argc != 2) {
+    cerr << "Usage:  " << argv[0] << " ADDRESS_BOOK_FILE" << endl;
+    return -1;
+  }
+
+  tutorial::AddressBook address_book;
+
+  {
+    // Read the existing address book.
+    fstream input(argv[1], ios::in | ios::binary);
+    if (!address_book.ParseFromIstream(&input)) {
+      cerr << "Failed to parse address book." << endl;
+      return -1;
+    }
+  }
+
+  ListPeople(address_book);
+
+  // Optional:  Delete all global objects allocated by libprotobuf.
+  google::protobuf::ShutdownProtobufLibrary();
+
+  return 0;
+}
